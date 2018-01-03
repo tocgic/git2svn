d650ba837395569343445ccadff092bd03f34eda
tocgic <jeongyon.cho@gmail.com>
2018-01-03 18:47:03 +0900

    git commit hash tag 추가.
    svn 커밋시, git commit hash tag를 추가 하여, 나중에 재 수행할 때, svn의 마지막 git hash tag를 확인하여, 이후부터 진행하도록 처리 함

--- git2svn.sh
+++ git2svn.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 BASE_DIR=`pwd`
+#GIT_DIR="/Users/gc/Temp/git_repo"
 GIT_DIR=$1
+#SVN_DIR="/Users/gc/Temp/svn_repo"
 SVN_DIR=$2
 
 # The SVN_AUTH variable can be used in case you need credentials to commit
 #SVN_AUTH="--username guilherme.chapiewski@gmail.com --password XPTO"
 SVN_AUTH=""
+
+GIT_COMMIT_HASH='GitCommitHash:'
+GIT_BRANCH_NAME='master'
 
 function svn_checkin {
 	echo '... adding files'
@@ -35,16 +40,48 @@ function svn_checkin {
 	echo '... finished adding files'
 }
 
+function clean_up {
+	echo "... clean_up";
+	cd $SVN_DIR && svn $SVN_AUTH revert . -R && svn $SVN_AUTH cleanup . --remove-unversioned && cd $BASE_DIR;
+	cd $GIT_DIR && git checkout $GIT_BRANCH_NAME && cd $BASE_DIR;
+	echo "... clean_up finished!";
+}
+
+function svn_last_git_commit_hash {
+	echo "... check last git commit hash";
+	cd $SVN_DIR && svn $SVN_AUTH update && cd $BASE_DIR;
+	lastGitCommitHash=`cd ${SVN_DIR} && svn ${SVN_AUTH} log --xml -l 1 | grep ${GIT_COMMIT_HASH} | sed s/${GIT_COMMIT_HASH}//g | sed s/\<[/?]msg\>//g && cd ${BASE_DIR}`;
+	hasgLength=${#lastGitCommitHash};
+	if [[ "$hasgLength" != '40' ]]; then
+		lastGitCommitHash='';
+	fi
+	echo "... check last git commit hash : [$lastGitCommitHash]";
+}
+
 function svn_commit {
 	echo "... committing -> [$author]: $msg";
 	cd $SVN_DIR && svn $SVN_AUTH commit -m "[$author]: $msg" && cd $BASE_DIR;
 	echo '... committed!'
 }
 
-for commit in `cd $GIT_DIR && git rev-list --all --reverse && cd $BASE_DIR`; do 
+# STEP 1. clean
+clean_up;
+
+# STEP 2. check last gitCommitHash on svn
+svn_last_git_commit_hash;
+if [[ "$lastGitCommitHash" != '' ]]; then
+	revListQuery="${lastGitCommitHash}..HEAD";
+else
+	revListQuery="--all";
+fi
+
+# STEP 3. commit in looping
+for commit in `cd $GIT_DIR && git rev-list $GIT_BRANCH_NAME $revListQuery --reverse && cd $BASE_DIR`; do 
 	echo "Committing $commit...";
 	author=`cd ${GIT_DIR} && git log -n 1 --pretty=format:%an ${commit} && cd ${BASE_DIR}`;
 	msg=`cd ${GIT_DIR} && git log -n 1 --pretty=format:%s ${commit} && cd ${BASE_DIR}`;
+	# add msg (GitCommitHash:{commit})
+	msg="${msg}"$'\n'$'\n'"${GIT_COMMIT_HASH}${commit}";
 	
 	# Checkout the current commit on git
 	echo '... checking out commit on Git'
